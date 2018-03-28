d086ef6b467ab61c64544ddb4a91a1bad13ccc7c
aledefra <37877991+aledefra@users.noreply.github.com>
2018-03-28 20:03:52 +0200

    aggiornamento

--- functions.php
+++ functions.php
@@ -12,9 +12,12 @@
         die("Connection failed: " . $conn->connect_error);
     }
 
-    $sql = "SELECT fatture.*, ditte.nomeDitta, controparti.nome
+    $sql = "SELECT fatture.dataCaric, fatture.tipo_doc, fatture.dataFatt, fatture.nFatt, fatture.nProtocollo, fatture.totFatt,
+fatture.stato,
+	controparti.nomeForn, ditte.nomeDitta
     FROM fatture, ditte, controparti
-    WHERE fatture.ditta = ditte.codiceDitta AND fatture.controparte=controparti.id";
+    WHERE fatture.ditta = ditte.codiceDitta AND fatture.controparte=controparti.id
+    ORDER BY fatture.id";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
@@ -22,27 +25,79 @@
         while($row = $result->fetch_assoc()) {
           print("<tr>");  /*inizio riga*/
           print("<td>".$row["nomeDitta"]."</td>"); /* ditta */
-          $sqlDate = strtotime($row["dataCaric"]);  /*rende il formato */
-          $formatDate = date("d/m/Y", $sqlDate);   /*della data normale*/
-          print("<td>".$formatDate."</td>"); /* data caricamento */
-          print("<td>Fattura ricevuta</td>"); /* tipo documento */
-          $sqlDate = strtotime($row["dataFatt"]);   /*rende il formato */
-          $formatDate = date("d/m/Y", $sqlDate);   /*della data normale*/
-          print("<td>".$formatDate."</td>"); /* data documento */
+          if ($row["dataCaric"]) {
+            $sqlDate = strtotime($row["dataCaric"]);  /*rende il formato */
+            $formatDate = date("d/m/Y", $sqlDate);   /*della data normale*/
+            print("<td>".$formatDate."</td>"); /* data caricamento */
+          } else {
+            print("<td></td>");
+          }
+          print("<td>".$row["tipo_doc"]."</td>"); /* tipo documento */
+          if ($row["dataFatt"]) {
+            $sqlDate = strtotime($row["dataFatt"]);   /*rende il formato */
+            $formatDate = date("d/m/Y", $sqlDate);   /*della data normale*/
+            print("<td>".$formatDate."</td>"); /* data documento */
+          } else {
+            print("<td></td>");
+          }
           print("<td>".$row["nFatt"]."</td>"); /* numero fattura */
           print("<td>".$row["nProtocollo"]."</td>"); /* protocollo */
-          print("<td>".$row["nome"]."</td>");  /* controparte */
+          print("<td>".$row["nomeForn"]."</td>");  /* controparte */
           print("<td>".$row["totFatt"]."€</td>");  /* totale */
-          print("<td class=\"text-center\" style=\"width: 30px;\">"); /* icona di stato */
-            print("<i class=\"red fa fa-times\"></i> Da completare");
-          print("</td>");
-          print("<td style=\"width: 50px;\">");  /* icona modifica/visualizza */
-            print("<div style=\"display: inline-block; min-width: 32px;\">");
-              print("<a class=\"btn btn-xs btn-info\" data-toggle=\"tooltip\" data-placement=\"top\" data-title=\"Modifica\" href=\"#\">");
-                print("<i class=\"ace-icon fa fa-pencil bigger-120\"></i>");
-              print("</a>");
-            print("</div>");
-          print("</td>");
+          switch ($row["stato"]) {  /* icona di stato */
+            case "Da registrare": {
+              print("<td class=\"text-center\" style=\"width: 30px;\">");
+                print("<i class=\"red fa fa-times\"></i> Da registrare");
+              print("</td>");
+              print("<td style=\"width: 50px;\">");  /* icona modifica/visualizza */
+                print("<div style=\"display: inline-block; min-width: 32px;\">");
+                  print("<a class=\"btn btn-xs btn-info\" data-toggle=\"tooltip\" data-placement=\"top\" data-title=\"Modifica\" href=\"#\">");
+                    print("<i class=\"ace-icon fa fa-pencil bigger-120\"></i>");
+                  print("</a>");
+                print("</div>");
+              print("</td>");
+              break;
+            }
+            case "Nuovo": {
+              print("<td class=\"text-center\" style=\"width: 30px;\">");
+                print("<i class=\"red fa fa-times\"></i> Nuovo");
+              print("</td>");
+              print("<td style=\"width: 50px;\">");  /* icona modifica/visualizza */
+                print("<div style=\"display: inline-block; min-width: 32px;\">");
+                  print("<a class=\"btn btn-xs btn-info\" data-toggle=\"tooltip\" data-placement=\"top\" data-title=\"Modifica\" href=\"#\">");
+                    print("<i class=\"ace-icon fa fa-pencil bigger-120\"></i>");
+                  print("</a>");
+                print("</div>");
+              print("</td>");
+              break;
+            }
+            case "Registrato": {
+              print("<td class=\"text-center\" style=\"width: 30px;\">");
+                print("<i class=\"red fa fa-times\"></i> Registrato");
+              print("</td>");
+              print("<td style=\"width: 50px;\">");  /* icona modifica/visualizza */
+                print("<div style=\"display: inline-block; min-width: 32px;\">");
+                  print("<a class=\"btn btn-xs btn-info\" data-toggle=\"tooltip\" data-placement=\"top\" data-title=\"Modifica\" href=\"#\">");
+                    print("<i class=\"ace-icon fa fa-pencil bigger-120\"></i>");
+                  print("</a>");
+                print("</div>");
+              print("</td>");
+              break;
+            }
+            case "Rifiutato": {
+              print("<td class=\"text-center\" style=\"width: 30px;\">");
+                print("<i class=\"red fa fa-times\"></i> Rifiutato");
+              print("</td>");
+              print("<td style=\"width: 50px;\">");  /* icona modifica/visualizza */
+                print("<div style=\"display: inline-block; min-width: 32px;\">");
+                  print("<a class=\"btn btn-xs btn-info\" data-toggle=\"tooltip\" data-placement=\"top\" data-title=\"Modifica\" href=\"#\">");
+                    print("<i class=\"ace-icon fa fa-pencil bigger-120\"></i>");
+                  print("</a>");
+                print("</div>");
+              print("</td>");
+              break;
+            }
+          }          
           print("</tr>");
         }
     } else {
